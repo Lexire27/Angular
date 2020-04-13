2cb4172d22bbdd84630851ec1bf2f30341966f4c
Lexire <62467784+Lexire27@users.noreply.github.com>
2020-04-13 11:49:17 +0200

    Add files via upload

---
+++ eclipse-workspace/spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,14 @@
+/* Populate tabla clientes */
+
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Andrés', 'Guzmán', 'porfesorbolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Kiko', 'Parra','kikobolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Shrek', 'Johnson','shrekbolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Harley', 'Quenn','harleybolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Linterna', 'Verde','linternabolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Charlie', 'Chaplin','charliebolsa@ideas.com', '2020-03-02');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jesus', 'Sommer','jesussommer.bolsa@ideas.com', '2020-04-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Carlos', 'Venet','carlosvenet.bolsa@ideas.com', '2020-04-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Chris', 'Hurtado','chrishurtado.bolsa@ideas.com', '2020-04-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jesse', 'Soffer', 'jessesoffer.bolsa@ideas.com', '2020-04-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Vin', 'Diesel', 'vindiesel.bolsa@ideas.com', '2020-04-08');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Ben', 'Affleck', 'benaffleck.bolsa@ideas.com', '2020-04-08');
